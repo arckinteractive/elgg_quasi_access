b9bdba5e1aee59dbcab65d29ef8a213141638166
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2013-11-22 09:43:16 +0100

    ACESS_FRIENDS should be handled separately

--- lib/hooks.php
+++ lib/hooks.php
@@ -30,7 +30,7 @@ function elgg_quasi_access_prepare_action_values($hook, $type, $return, $params)
 		$query = http_build_query(array(
 			$input_name => $metacollection_id
 		));
-	
+
 		$arr = array();
 		parse_str($query, $arr);
 
@@ -44,13 +44,14 @@ function elgg_quasi_access_prepare_action_values($hook, $type, $return, $params)
 	}
 
 	set_input('__quasi_access_inputs', null);
-	
+
 	return $return;
 }
 
-
 /**
- * Collapsing metacollections into their components
+ * We need to allow access to entities that have a metacollection access id:
+ * - if metacollection contains ACCESS_FRIENDS and the current user is friends with the metacollection owner
+ * - if metacollection contains any of the ACLs that the current user is allowed to see
  * 
  * @global type $QUASI_ACCESS_IGNORE_SQL_SUFFIX Flag to prevent infinite loops
  * @global type $QUASI_ACCESS_ACL_CACHE Cache to avoid duplicate calls to the DB
@@ -65,7 +66,7 @@ function elgg_quasi_access_collections_read($hook, $type, $return, $params) {
 
 	$dbprefix = elgg_get_config('dbprefix');
 
-	$user_guid = elgg_extract('user_id', $params);
+	$user_guid = sanitize_int(elgg_extract('user_id', $params));
 
 	if (!isset($QUASI_ACCESS_ACL_CACHE[$user_guid])) {
 
@@ -77,22 +78,51 @@ function elgg_quasi_access_collections_read($hook, $type, $return, $params) {
 			return $return;
 		}
 
-		// We need to grab metadata from the metacollection entity
-		// where 'member_acl' metadata value contains any of the
-		// values from the current $return
-
 		$metastring_name_id = get_metastring_id('member_acl');
-		foreach ($return as $id) {
+
+		// We need to grab metacollection access ids for metacollections that contain ACCESS_FRIENDS
+		// and where the current user is a friend of the metacollecion owner
+
+		$metastring_value_id = get_metastring_id(ACCESS_FRIENDS);
+		if (!$metastring_value_id) {
+			$metastring_value_id = add_metastring(ACCESS_FRIENDS);
+		}
+
+		$query = "SELECT DISTINCT(ac.id) as acl"
+				. " FROM {$dbprefix}access_collections ac"
+				. " JOIN {$dbprefix}entities e ON e.guid = ac.owner_guid" // metacollection entity
+				. " JOIN {$dbprefix}metadata md ON md.entity_guid = e.guid AND md.name_id = $metastring_name_id" // metacollection member acl metadata
+				. " WHERE  md.value_id = $metastring_value_id AND e.owner_guid IN "
+						. " (SELECT r.guid_one "
+							. " FROM {$dbprefix}entity_relationships r"
+							. " WHERE r.relationship='friend' AND r.guid_two=$user_guid)";
+
+		$acls = get_data($query);
+		foreach ($acls as $acl) {
+			if ($acl->acl) {
+				$return[] = (int) $acl->acl;
+			}
+		}
+
+		// We need to grab metacollection access ids for metacollections that contain any
+		// of the current read access ids (except implicit acls)
+
+		$implicit_acls = array(ACCESS_PRIVATE, ACCESS_FRIENDS, ACCESS_LOGGED_IN, ACCESS_PUBLIC);
+		$allowed_acl_ids = array_diff($return, $implicit_acls);
+
+		foreach ($allowed_acl_ids as $id) {
 			$metastring_value_id = get_metastring_id($id);
 			if (!$metastring_value_id) {
 				$metastring_value_id = add_metastring($id);
 			}
-			$acl_ids[] = sanitize_int($id);
 			$metastring_value_ids[] = sanitize_int($metastring_value_id);
 		}
 
-		if (empty($acl_ids)
-				|| empty($metastring_value_ids)) {
+		foreach ($return as $id) {
+			$acl_ids[] = sanitize_int($id);
+		}
+
+		if (empty($acl_ids) || empty($metastring_value_ids)) {
 			return $return;
 		}
 
@@ -107,7 +137,9 @@ function elgg_quasi_access_collections_read($hook, $type, $return, $params) {
 
 		$acls = get_data($query);
 		foreach ($acls as $acl) {
-			$return[] = (int) $acl->acl;
+			if ($acl->acl) {
+				$return[] = (int) $acl->acl;
+			}
 		}
 
 		$QUASI_ACCESS_IGNORE_SQL_SUFFIX = false;
@@ -116,6 +148,6 @@ function elgg_quasi_access_collections_read($hook, $type, $return, $params) {
 	} else {
 		$return = $QUASI_ACCESS_ACL_CACHE[$user_guid];
 	}
-	
+
 	return $return;
 }
